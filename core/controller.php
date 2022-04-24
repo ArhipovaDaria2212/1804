4b299239b91070483275ca736761eb117312b3d2
Arhipova Daria <daria221220042017@gmail.com>
2022-04-24 19:39:07 +0700

    mvc

--- core/controller.php
+++ core/controller.php
@@ -1,8 +1,11 @@
 <?php
-	namespace core;
+	namespace Core;
 	
 	class Controller
 	{
+		protected $layout = 'default';
 		
+		protected function render($view, $data = []) {
+			return new Page($this->layout, $this->title, $view, $data);
+		}
 	}
-?>
--- project/controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace project;
-	
-	class Controller
-	{
-		
-	}
-?>
