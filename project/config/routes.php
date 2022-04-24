ede93bf4847b624073834fa60f9096c78155b453
Arhipova Daria <daria221220042017@gmail.com>
2022-04-24 20:08:29 +0700

    93 глава

--- project/config/routes.php
+++ project/config/routes.php
@@ -2,6 +2,11 @@
 	use \Core\Route;
 	
 	return [
-		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
+		new Route('/my-test1/', 'test', 
+			'act1'), 
+		new Route('/my-test2/', 'test', 
+			'act2'), 
+		new Route('/my-test3/', 'test', 
+			'act3'), 	
 	];
 ?>
